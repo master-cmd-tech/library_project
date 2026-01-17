e641675a14dc65613d4f8a10fc228b056d04dbd7
master-cmd-tech <abdrahmanov01122006@gmail.com>
2026-01-17 12:09:15 +0500

    commit

---
+++ src/edu/oop/db/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package edu.oop.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String USER = "postgres.wcfmjbrccdtmqnhnscla";
+    private static final String PASSWORD = "i86Vs4fRZiqIdgkp"; // ← DATABASE PASSWORD
+
+    private DatabaseConnection() {
+        // no instances
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
