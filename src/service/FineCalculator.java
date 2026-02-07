e17cecd68bf43009a123ca11a3cb7732386a2a60
Kaisar Yessenov <thetopnotchmma@gmail.com>
2026-02-07 11:13:38 +0500

    Kaisar Yessenov changed FineCalculator

--- src/service/FineCalculator.java
+++ src/service/FineCalculator.java
@@ -1,16 +1,16 @@
 package service;
 
+import config.LibraryConfig;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class FineCalculator {
 
-    private static final double DAILY_FINE = 1.5;
-
     public double calculateFine(LocalDate dueDate, LocalDate returnDate) {
         if (returnDate.isAfter(dueDate)) {
             long daysLate = ChronoUnit.DAYS.between(dueDate, returnDate);
-            return daysLate * DAILY_FINE;
+            return daysLate * LibraryConfig.getInstance().getDailyFine();
         }
         return 0;
     }
